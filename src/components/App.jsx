f64ffec5aba651c7c8ad4a54a8a025fb6ae9cc14
Josh <mrjoshuasperry@gmail.com>
2021-04-22 05:28:50 -0400

    Add a main menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
+import MainMenu from "./menus/main-menu/MainMenu";
 import "../sass/base.scss";
 import "./App.scss";
 
 class App extends React.Component {
     render() {  
-        return null;
+        return (
+            <MainMenu/>
+        );
     }
 }
 
