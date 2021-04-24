b0fefe1068f08e5204bf077370cc5c9f6c3cc672
Josh <mrjoshuasperry@gmail.com>
2021-04-24 16:46:47 -0400

    Remove react hot loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { hot } from "react-hot-loader";
 import MainMenu from "./menus/main-menu/MainMenu";
 import "../sass/base.scss";
 import "./App.scss";
@@ -13,4 +12,4 @@ const App = () => {
         <MainMenu/>
     </>);
 };
-export default hot(module)(App);
+export default App;
